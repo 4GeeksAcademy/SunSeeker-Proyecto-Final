197d9e77d67f98e55fd7e43fc40ae576e3f955dd
Camila Gabriele <camilagabriele@outlook.com.ar>
2026-03-06 19:21:58 +0000

    cambios home

--- src/front/pages/Home/Home.jsx
+++ src/front/pages/Home/Home.jsx
@@ -35,7 +35,7 @@ export const Home = () => {
     // }, [])
 
     return (
-        <div >
+        <div className=" d-flex justify-content-center align-item-center mt-5">
         <PhaserGame/>
         </div>
     );
