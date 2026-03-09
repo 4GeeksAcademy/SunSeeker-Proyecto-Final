c67ffe9170d4b21874794c5cb09957181068749e
AxelAFV <axel.fernandezvalverde@gmail.com>
2026-03-09 21:25:12 +0000

    Logica ranking, falta pull request de routes_fetch para subir archivos a la db y verificar el funcionamiento correcto

--- src/front/pages/Ranking/Ranking.jsx
+++ src/front/pages/Ranking/Ranking.jsx
@@ -1,72 +1,48 @@
+import { useEffect, useState } from "react";
 import "./Ranking.css";
+import { queryRanking } from "../../Service/BackEndServices";
 
 export const Ranking = () => {
+
+    const [ranking, setRanking] = useState([])
+
+
+    const fetchData = async () => {
+        try {
+            const data = await queryRanking();
+            const top5 = data.sort((a, b) => b.score - a.score).slice(0, 5);
+            setRanking(top5);   
+        } 
+        catch (error){
+            console.log("error:", error.message);
+        } 
+    };
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
     return (
         <>
             <div className="ranking-page-container">
                 <p className="subtitulo mt-5"> RANKING </p>
                 <div className="ranking">
-                    <div className="player-card top3">
-                        <div className="rank-number"></div>
-                        <div className="cat-avatar">🐱</div>
-                        <div className="player-info">
-                            <div className="player-name">michi_name</div>
-                            <div className="score-bar-bg">
-                                <div className="score-bar-fill"></div>
+                    {ranking.map(({ id, michi_name, score }, index) => (
+                            <div key={id} className={`player-card ${index < 3 ? "top3" : ""}`}>
+                                <div className="rank-number"></div>
+                                <div className="cat-avatar">🐱</div>
+                                <div className="player-info">
+                                    <div className="player-name">{michi_name}</div>
+                                    <div className="score-bar-bg">
+                                        <div className="score-bar-fill"></div>
+                                    </div>
+                                </div>
+                                <div className="score">{score}</div>
                             </div>
-                        </div>
-                        <div className="score">148,320</div>
-                    </div>
-
-                    <div className="player-card top3">
-                        <div className="rank-number"></div>
-                        <div className="cat-avatar">🐱</div>
-                        <div className="player-info">
-                            <div className="player-name">michi_name</div>
-                            <div className="score-bar-bg">
-                                <div className="score-bar-fill"></div>
-                            </div>
-                        </div>
-                        <div className="score">121,880</div>
-                    </div>
-
-                    <div className="player-card top3">
-                        <div className="rank-number"></div>
-                        <div className="cat-avatar">🐱</div>
-                        <div className="player-info">
-                            <div className="player-name">michi_name</div>
-                            <div className="score-bar-bg">
-                                <div className="score-bar-fill"></div>
-                            </div>
-                        </div>
-                        <div className="score">98,540</div>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="rank-number">4</div>
-                        <div className="cat-avatar">🐱</div>
-                        <div className="player-info">
-                            <div className="player-name">michi_name</div>
-                            <div className="score-bar-bg">
-                                <div className="score-bar-fill"></div>
-                            </div>
-                        </div>
-                        <div className="score">74,210</div>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="rank-number">5</div>
-                        <div className="cat-avatar">🐱</div>
-                        <div className="player-info">
-                            <div className="player-name">michi_name</div>
-                            <div className="score-bar-bg">
-                                <div className="score-bar-fill"></div>
-                            </div>
-                        </div>
-                        <div className="score">61,090</div>
-                    </div>
+                        ))
+                    }
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
